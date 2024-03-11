77b556aad6094f33ea682619ef04e061be48461f
Icetea0113 <v2msyo@naver.com>
2024-03-11 12:59:57 +0900

    verson__1.0.0__

---
+++ turning_test.py
@@ -0,0 +1,126 @@
+import mmg_coefficients
+import mmg_equations_KLVCC2
+import mmg_equations_S175
+import numpy as np
+from scipy.interpolate import interp1d
+import matplotlib.pyplot as plt
+
+def main():
+    ship_type = mmg_coefficients.Ship("empty", mmg_coefficients.PrincipalDimensions(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0), mmg_coefficients.ManeuveringParams(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,0,0,0,0,0,0,0))
+
+    print("Excute Turning Test")
+
+    print(
+"**************************\n\
+1. S175 Container Ship\n\
+2. KVLCC2 Tanker\n\
+**************************")
+    
+    ship_type_str = input("Enter ship type: ")
+    if ship_type_str == "1":
+        ship_type = mmg_coefficients.S175
+        print(ship_type)
+        n_const = 10.05
+        max_δ_rad = -35 * np.pi / 180.0  # [rad]
+        rudder_rate = -12 * np.pi / 180.0
+        u0 = 0.89
+
+    elif ship_type_str == "2":
+        ship_type = mmg_coefficients.KVLCC2
+        print(ship_type)
+        n_const = 11.8
+        max_δ_rad = -35 * np.pi / 180.0  # [rad]
+        rudder_rate = -11.8 * np.pi / 180.0
+        u0 = 1.1767
+    else:
+        Exception("Invalid ship type")
+
+    # print("************ debuging mode ************")
+    # ship_type = mmg_coefficients.KVLCC2
+    # n_const = 11.8
+    # # steering_rate = 1.76 * 4  # [°/s]
+    # max_δ_rad = -35 * np.pi / 180.0  # [rad]
+    # rudder_rate = -11.8 * np.pi / 180.0
+    # u0 = 1.1767
+
+    duration = 200  # [s]
+
+    growing_rate = 10
+    sampling = duration * growing_rate
+    time_list = np.linspace(0.00, duration, sampling)
+    δ_rad_list = [0] * sampling
+    for i in range(1,sampling):
+        if np.abs(δ_rad_list[i-1]) >= np.abs(max_δ_rad):
+            δ_rad_list[i] = max_δ_rad  
+        else:
+            δ_rad_list[i] = δ_rad_list[i-1] + rudder_rate * (1 / growing_rate)
+            if np.abs(δ_rad_list[i]) > np.abs(max_δ_rad):
+                δ_rad_list[i] = max_δ_rad
+    sol = 0
+    npm_list = np.array([n_const for i in range(sampling)])
+    if ship_type.name == "KVLCC2":
+        sol = mmg_equations_KLVCC2.simulate(ship_type, time_list,δ_rad_list,npm_list, u0 = u0, v0=0.0, r0=0.0, method="RK45")
+    elif ship_type.name == "S175":
+        sol = mmg_equations_S175.simulate(ship_type, time_list,δ_rad_list,npm_list, u0 = u0, v0=0.0, r0=0.0, method="RK45")
+
+    if sol == 0:
+        Exception("Simulation failed")
+
+    result = sol.sol(time_list)
+
+    ship_type.register_simulation_result(time_list, result[0], result[1], result[2], result[3], result[4], result[5])
+
+    ship_type.draw_xy_trajectory(save_fig_path="trajectory.png")
+
+    ship_type.draw_chart(
+        "time",
+        "u",
+        xlabel="time [sec]",
+        ylabel=r"$u$" + " [m/s]",
+        save_fig_path="u_time_series.png",
+        x_lim=[0, 100, 20],
+        y_lim=[0.3, 1.0, 0.1]
+    )
+
+    ship_type.draw_chart(
+        "time",
+        "v",
+        xlabel="time [sec]",
+        ylabel=r"$v$" + " [m/s]",
+        save_fig_path="v_time_series.png",
+        x_lim=[0, 100, 20],
+        y_lim=[0, 0.2, 0.05]
+    )
+
+    ship_type.draw_chart(
+        "time",
+        "r",
+        xlabel="time [sec]",
+        ylabel=r"$r$" + " [m/s]",
+        save_fig_path="r_time_series.png",
+        x_lim=[0, 100, 20],
+        y_lim=[-7, 0, 1]
+    )
+
+    ship_type.draw_chart(
+        "time",
+        "beta",
+        xlabel="time [sec]",
+        ylabel=r"$r$" + " [m/s]",
+        save_fig_path="beta_time_series.png",
+        x_lim=[0, 100, 20],
+        y_lim=[-15, 0, 5]
+    )
+    for i in range(len(ship_type.psi)):
+        if np.abs(ship_type.psi[i]) >= 90 * np.pi / 180:
+            print(ship_type.x[i]/ship_type.principal_dimensions.Lpp)
+            break
+    for i in range(len(ship_type.psi)):
+        if np.abs(ship_type.psi[i]) >= 180 * np.pi / 180:
+            print(ship_type.y[i]/ship_type.principal_dimensions.Lpp)
+            break
+        
+    plt.show()
+
+if __name__ == "turning_test":
+    print("Turning Test is ready")
---
+++ zig_zag_test.py
@@ -0,0 +1,141 @@
+import mmg_coefficients
+import mmg_equations_KLVCC2
+import mmg_equations_S175
+import numpy as np
+from scipy.interpolate import interp1d
+import matplotlib.pyplot as plt
+#import pandas as pd
+
+def main():
+    ship_type = mmg_coefficients.Ship("empty", mmg_coefficients.PrincipalDimensions(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0), mmg_coefficients.ManeuveringParams(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,0,0,0,0,0,0,0))
+
+    print("Excute Zig Zag Test")
+
+    duration = 100
+    num_of_sampling = 20000
+    time_list = np.linspace(0.00, duration, num_of_sampling)
+
+    print(
+"**************************\n\
+1. S175 Container Ship\n\
+2. KVLCC2 Tanker\n\
+**************************")
+    
+    ship_type_str = input("Enter ship type number: ")
+    if ship_type_str == "1":
+        ship_type = mmg_coefficients.S175
+        target_δ_rad = 10.0 * np.pi / 180.0
+        target_ψ_rad_deviation = 10 * np.pi / 180.0
+        n_const = 7.54 # [rpm]
+        npm_list = np.array([n_const for i in range(num_of_sampling)])
+        u0 = 0.879
+
+    elif ship_type_str == "2":
+        ship_type = mmg_coefficients.KVLCC2
+        target_δ_rad = 20.0 * np.pi / 180.0
+        target_ψ_rad_deviation = 20 * np.pi / 180.0
+        n_const = 11.8  # [rpm]
+        npm_list = np.array([n_const for i in range(num_of_sampling)])
+        u0 = 1.1767
+
+    # print("************ debuging mode ************")
+    # ship_type = mmg_coefficients.KVLCC2
+
+    # target_δ_rad = 20.0 * np.pi / 180.0
+    # target_ψ_rad_deviation = 20 * np.pi / 180.0
+    # duration = 80
+    # num_of_sampling = 8000
+    # time_list = np.linspace(0.00, duration, num_of_sampling)
+    # n_const = 11.8  # [rpm]
+    # npm_list = np.array([n_const for i in range(num_of_sampling)])
+    # u0 = 1.1767
+
+
+
+    if ship_type_str == "2":
+        δ_list, u_list, v_list, r_list, x_list, y_list, ψ_list = mmg_equations_KLVCC2.zigzag_test_mmg_3dof(
+            ship_type,
+            target_δ_rad,
+            target_ψ_rad_deviation,
+            time_list,
+            npm_list,
+            u0=u0,
+            δ_rad_rate=11.8* np.pi / 180,
+        )
+        ship = mmg_coefficients.KVLCC2
+        ship.load_simulation_result(time_list, u_list, v_list, r_list)
+        ship.δ = δ_list
+        ship.npm = npm_list
+
+        ship2 = mmg_coefficients.KVLCC2
+        ship2.register_simulation_result(time_list, u_list, v_list, r_list, x_list, y_list, ψ_list)
+        ship2.δ = δ_list
+        ship2.npm = npm_list
+
+    elif ship_type_str == "1":
+        δ_list, u_list, v_list, r_list, x_list, y_list, ψ_list = mmg_equations_S175.zigzag_test_mmg_3dof(
+            ship_type,
+            target_δ_rad,
+            target_ψ_rad_deviation,
+            time_list,
+            npm_list,
+            u0=u0,
+            δ_rad_rate=12.0* np.pi / 180,
+        )
+        ship = mmg_coefficients.S175
+        ship.load_simulation_result(time_list, u_list, v_list, r_list)
+        ship.δ = δ_list
+        ship.npm = npm_list
+
+        ship2 = mmg_coefficients.S175
+        ship2.register_simulation_result(time_list, u_list, v_list, r_list, x_list, y_list, ψ_list)
+        ship2.δ = δ_list
+        ship2.npm = npm_list
+
+    # plt.plot(time_list, list(map(lambda psi: psi * 180 / np.pi, ship.psi)), color="limegreen")
+    # plt.plot(time_list, list(map(lambda δ: δ * 180 / np.pi, ship.δ)),color="limegreen")
+    # plt.xlim(0, 200)
+    # plt.ylim(-30, 30)
+    print(max(ship.psi[0:40000])* 180 / np.pi, min(ship.psi[0:70000])* 180 / np.pi)
+
+    ship2.draw_chart(
+        "time",
+        "u",
+        xlabel="time [sec]",
+        ylabel=r"$u$" + " [m/s]",
+        save_fig_path="zigzag_u_time_series.png",
+        x_lim=[0,100,10],
+        y_lim=[0.6,1.4,0.2]
+    )
+
+    # ship2.draw_chart(
+    #     "time",
+    #     "F_N",
+    #     xlabel="time [sec]",
+    #     ylabel=r"$v$" + " [m/s]",
+    #     save_fig_path="zigzag_v_time_series.png",
+    #     x_lim=[0,200,10],
+    #     y_lim=[-0.4,0.4,0.2]
+    # )
+
+    ship2.draw_chart(
+        "time",
+        "r",
+        xlabel="time [sec]",
+        ylabel=r"$r$" + " [m/s]",
+        save_fig_path="zigzag_r_time_series.png",
+        x_lim=[0,100,10],
+        y_lim=[-4,4,2]
+    )
+    # df = pd.DataFrame(time_list, columns=["time"])
+    # df["angle"] = list(map(lambda psi: psi * 180 / np.pi, ship.psi))
+    # df.to_csv("zigzag.csv", index=False)
+
+    plt.plot(time_list , ship.F_N, color="limegreen")
+
+    plt.show()
+
+if __name__ == "zig_zag_test":
+    print("Zig Zag Test is ready")
+elif __name__ == "main":
+    print("main~!")
